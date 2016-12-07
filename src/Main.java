76e88f55770d5654ec68db686853ea4a996e8a12
Mateusz Dobrowolski <mateusz.dobrowolski@britenet.com.pl>
2016-12-07 11:32:53 +0100

    feature/test part1

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World! jakas zmiana dodana");
     }
+
+    public void featureTest(){
+        System.out.println("Feature/Test");
+    }
 }
